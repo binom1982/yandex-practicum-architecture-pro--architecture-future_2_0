af3681e62c31be48a11f1fe9099ed9a2c18ddb9e
binom1982 <69306408+binom1982@users.noreply.github.com>
2026-06-21 20:16:08 +0900

    мелкие правки

--- Task1Advanced/modules/vm/main.tf
+++ Task1Advanced/modules/vm/main.tf
@@ -1,3 +1,14 @@
+terraform {
+  required_version = ">= 1.0.0"
+  
+  required_providers {
+    yandex = {
+      source  = "yandex-cloud/yandex"
+      version = ">= 0.100.0"
+    }
+  }
+}
+
 # --- Сеть ---
 resource "yandex_vpc_network" "network" {
   count       = var.subnet_id == null ? 1 : 0
